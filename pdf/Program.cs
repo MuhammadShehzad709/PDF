b73dbe30a75a3743d35bfa814b409447dc69129a
MuhammadShehzad709 <muhammadshehzaddeveloper@gmail.com>
2025-05-23 11:31:12 -0700

    adding Port to Progrma.cs

--- pdf/Program.cs
+++ pdf/Program.cs
@@ -8,7 +8,7 @@ namespace pdf
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            builder.WebHost.UseUrls("http://0.0.0.0:8080");
             // Add services to the container.
             builder.Services.AddControllers();
             builder.Services.AddScoped<IFileUploadService, FileUploadService>();
